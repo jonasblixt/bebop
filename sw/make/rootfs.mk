7eecb550141d12fd05d462e7507375db030a5260
Jonas Blixt <jonpe960@gmail.com>
2019-11-03 08:55:27 +0100

    Bump subrepos

--- sw/make/rootfs.mk
+++ sw/make/rootfs.mk
@@ -3,7 +3,7 @@ ROOTFS_DIR = $(TOP_DIR)/build/rootfs
 
 init_rootfs:
 	$(info Initializing rootfs)
-	$(shell rm -rf $(ROOTFS_DIR))
+	#$(shell rm -rf $(ROOTFS_DIR))
 	$(shell mkdir -p $(ROOTFS_DIR)/dev)
 	$(shell mkdir -p $(ROOTFS_DIR)/proc)
 	$(shell mkdir -p $(ROOTFS_DIR)/tmp)
